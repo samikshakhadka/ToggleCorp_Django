aae065154708bb7f768b6ebc64f53023407fca8e
Samiksha <swabhimankhadka2001@gmail.com>
2024-06-11 11:46:18 +0545

    base-code steup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--9dfxwc5rm$jg#6awbesc@(@$ctw7zezu_el7smb_$rf(eqf+k'
+SECRET_KEY = 'django-insecure-)uebr%^1&^5e+=o_=&p&erddsab(u6gd!e1$&=j2e8i@=-y916'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
